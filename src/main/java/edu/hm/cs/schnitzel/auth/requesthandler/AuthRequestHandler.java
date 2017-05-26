46449a07ced9bc6d551ededd1b6e9af9b9caf77a
truenicfel <dassler@hm.edu>
2017-05-26 10:24:13 +0200

    my part is done

--- src/main/java/edu/hm/cs/schnitzel/auth/requesthandler/AuthRequestHandler.java
+++ src/main/java/edu/hm/cs/schnitzel/auth/requesthandler/AuthRequestHandler.java
@@ -7,6 +7,7 @@
  */
 package edu.hm.cs.schnitzel.auth.requesthandler;
 
+import edu.hm.cs.schnitzel.auth.entities.User;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -27,79 +28,74 @@ import edu.hm.cs.schnitzel.auth.services.Service;
 @Path("/auth")
 public class AuthRequestHandler {
 
-	// Fields
+    // Fields
+    /**
+     * The service object (AuthService)
+     */
+    private final Service service = new AuthService();
 
-	/**
-	 * The service object (AuthService)
-	 */
-	private final Service service = new AuthService();
-
-	// Public Methods
-
+    // Public Methods
     /**
      * Validate token with authentification server.
-     * 
+     *
      * Accepts token as JSON
-     * 
+     *
      * @param token is the token to be validated
      * @return the response with code, message and content
      */
     @POST
     @Path("/token")
-	@Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response validateToken(final String token) {
-    	// Call service method and wait for result
-    	final Result result = getService().validateToken(token);
-    	// Build JSON-Content
-    	final JSONObject content = new JSONObject()
-    			.put("valid", 	result.getContent().isValid())
-    			.put("role", 	result.getContent().getUserRole());
-    	// Build JSON-Result
-    	final JSONObject json = new JSONObject()
-		    	.put("code",	result.getCode())
-		    	.put("message",	result.getMessage())
-		    	.put("content", content);
-		return Response.status(Response.Status.ACCEPTED).entity(json).build();
+        // Call service method and wait for result
+        final Result result = getService().validateToken(token);
+        // Build JSON-Content
+        final JSONObject content = new JSONObject()
+                .put("valid", result.getContent().isValid())
+                .put("role", result.getContent().getUserRole());
+        // Build JSON-Result
+        final JSONObject json = new JSONObject()
+                .put("code", result.getCode())
+                .put("message", result.getMessage())
+                .put("content", content);
+        return Response.status(Response.Status.ACCEPTED).entity(json).build();
     }
 
     /**
      * Request token from authentification server.
-     * 
+     *
      * Accepts username and password as JSON
-     * 
-     * @param username is the name of the user
-     * @param password is the password of the user
-     * 
+     *
+     * @param user The user who requests a token.
      * @return the response with code, message and content
      */
     @POST
     @Path("")
-	@Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response requestToken(final String username, final String password) {
-    	// Call service method and wait for result
-    	final Result result = getService().requestToken(username, password);
-    	// Build JSON-Content
-    	final JSONObject content = new JSONObject()
-    			.put("token", 	result.getContent().getToken())
-    			.put("role", 	result.getContent().getUserRole());
-    	// Build JSON-Result
-    	final JSONObject json = new JSONObject()
-		    	.put("code",	result.getCode())
-		    	.put("message",	result.getMessage())
-		    	.put("content", content);
-		return Response.status(Response.Status.ACCEPTED).entity(json).build();
+    public Response requestToken(User user) {
+        // Call service method and wait for result
+        final Result result = getService().requestToken(user);
+        // Build JSON-Content
+        final JSONObject content = new JSONObject()
+                .put("token", result.getContent().getToken())
+                .put("role", result.getContent().getUserRole());
+        // Build JSON-Result
+        final JSONObject json = new JSONObject()
+                .put("code", result.getCode())
+                .put("message", result.getMessage())
+                .put("content", content);
+        return Response.status(Response.Status.ACCEPTED).entity(json).build();
     }
 
     // Getter and Setter
-
     /**
      * Getter for service.
-     * 
+     *
      * @return the service object
      */
     private Service getService() {
-		return service;
-	}
+        return service;
+    }
 }
