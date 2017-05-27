c11b727721c7736312375df2a9e32b758c3b14ae
konopac <konopac@hm.edu>
2017-05-27 14:33:54 +0200

    Test is at the right position now

--- src/test/authTest/RestTest.java
+++ src/test/authTest/RestTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.hm.cs.schnitzel.auth.test;
+package authTest;
 
 import static org.junit.Assert.assertEquals;
 
